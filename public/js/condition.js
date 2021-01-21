d9e1428bfe8714437ec55aba92ee9c2452217b77
Beny <b.ntambwe97@gmail.com>
2021-01-21 11:18:49 +0100

    fin exo1 et 2

---
+++ public/js/condition.js
@@ -0,0 +1,31 @@
+// EXO1
+
+// let quest = prompt("Entrez un premier nombre")
+// let quest2 = prompt("Entrez un second nombre")
+// let quest3 = prompt("Entrez un troisième nombre")
+// if (quest == quest2 && quest == quest3) {
+//     console.log("Les trois variables sont identiques")
+// } else if (quest == quest2 || quest == quest3 || quest2 == quest3) {
+//     console.log("Deux des valeurs sont égales")
+// } else {
+//     console.log("Les trois variables sont différentes")
+// }
+
+
+// EXO 2
+
+// let quest = prompt("Quel temps fait-il dehors ?")
+// if (quest >= 22) {
+//     alert("Il y'a beaucoup de solaire auzordi, sortez en t-shirt")
+// } else {
+//     alert("Froid, va porter un manteau")
+// }
+// switch (quest) {
+//     case 'soleil' :
+//         console.log("Il y'a beaucoup de solaire auzordi, sortez en t-shirt")
+//         break;
+
+//     default:
+//         console.log("Froid, va porter un manteau")
+//         break;
+// }
